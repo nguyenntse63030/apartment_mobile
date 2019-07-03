35037695f4ec50a2a80a9bceefb71f4298c7fea9
danghuule1998 <danghuule1998@gmail.com>
2019-07-03 21:52:19 +0700

    sửa link đúng chuẫn

--- app/src/main/java/com/example/apartment/Api/RoomApi.java
+++ app/src/main/java/com/example/apartment/Api/RoomApi.java
@@ -7,6 +7,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface RoomApi {
-    @GET("room/{userId}")
+    @GET("room/userId/{userId}")
     Call<JsonElement> getRoomByUser(@Path("userId")String userId);
 }
